6363f84bfb6e9b7b87426f6a627a0e91ac5cc7ea
Kacper Łęczyński <kacper.leczynski@wpengine.com>
2025-09-15 15:55:46 +0200

    feat: allowedHosts added

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    // Use string suffixes; ".example.com" matches any host ending with that domain
+    allowedHosts: [
+      '.wpenginepowered.com',
+      '.js.wpenginepowered.com',
+    ],
+  },
 })
